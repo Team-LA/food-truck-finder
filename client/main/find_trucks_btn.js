542410506f9f4d90c88d64850bd1f41cf4622045
Eirik <kirie@Eiriks-MacBook-Air.local>
2016-04-12 17:04:43 -0700

    [Layout] Created file structure and pseudocode

---
+++ client/main/find_trucks_btn.js
@@ -0,0 +1,2 @@
+// sends geolocation to server
+// on response call map_display and truck_list with data
---
+++ client/routes.js
@@ -0,0 +1,14 @@
+main page view
+	route: '/'
+
+login page view 
+	route: '/login'
+
+sigup page view
+	route: '/signup'
+
+owner home page view
+	route: '/owner-form-view'
+
+owner success info added view
+	route: 'owner-profile'
