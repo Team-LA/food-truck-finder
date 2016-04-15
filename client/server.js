ed9c3c2c0a7eb4dc039720d6c334a093b668b441
Patrice Sandhu <pmsandhu@ucla.edu>
2016-04-14 23:42:53 -0700

    [Dependency] commiting changes made to client  webpack-server files before adding them to parent directory

--- client/server.js
+++ client/server.js
@@ -1,33 +1,32 @@
-var morgan = require('morgan');
-var path = require('path');
 var express = require('express');
+var app = express();
+
+var morgan = require('morgan');
+app.use(morgan('combined'));
+
 var webpack = require('webpack');
 var config = require('./webpack.config');
-
-var app = express();
 var compiler = webpack(config);
-
-app.use(morgan('combined'));
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }));
 
+var path = require('path');
 app.get('/css/bootstrap.min.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build/css/bootstrap.min.css'));
+  res.sendFile(path.join(__dirname, 'client/build/css/bootstrap.min.css'));
 });
 
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build/index.html'));
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
-  app.listen(3000, 'localhost', function (err) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-
-    console.log('Listening at http://localhost:3000');
+app.listen(3000, 'localhost', function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Listening at http://localhost:3000');
 });
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,22 +1,21 @@
-/* eslint-disable no-var */
 var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
   entry: [
-    './src/app'
+    '.client/src/app'
   ],
   devtool: 'eval-source-map',
   output: {
     path: __dirname,
-    filename: 'app.js',
+    filename: 'client/app.js',
     publicPath: '/js/'
   },
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: ['babel'],
-      include: path.join(__dirname, 'src')
+      include: path.join(__dirname, 'client/src')
     }]
   }
 };
